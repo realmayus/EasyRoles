698a886e41da71d2bcaf5c8b8de55d2c1f185a88
realmayus <realmayus@gmail.com>
2020-05-18 15:07:01 +0200

    changed logging mode to write (was "append")

--- EasyRoles.py
+++ EasyRoles.py
@@ -18,7 +18,7 @@ from firebase_admin import firestore  # for working with the database
 #############
 log_formatter = logging.Formatter('%(asctime)s %(levelname)s %(funcName)s(%(lineno)d) %(message)s')
 logFile = 'log.txt'
-my_handler = RotatingFileHandler(filename=logFile, mode='a', maxBytes=5*1024*1024, backupCount=2, encoding=None, delay=0)
+my_handler = RotatingFileHandler(filename=logFile, mode='w', maxBytes=5*1024*1024, backupCount=2, encoding=None, delay=0)
 my_handler.setFormatter(log_formatter)
 my_handler.setLevel(logging.INFO)
 logger = logging.getLogger("easyroles")
